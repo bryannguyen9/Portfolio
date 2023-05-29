94114f853aa7370ff07b949e27724beac6ab3803
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-05-28 21:00:54 -0700

    updated header.js need logic for actual contents of header

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -1,1 +1,12 @@
 // This component will represent the header section of portfolio.
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* Add your header content here */}
+    </header>
+  );
+}
+
+export default Header;
