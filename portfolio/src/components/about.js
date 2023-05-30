891c2b914f144eeeffe31537e9699eca65ad251e
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-05-29 21:45:49 -0700

    added content in about and contact files

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import profilePic from '../Assets/profilepic.jpg';
 
 function About() {
+  const aboutContent = {
+    title: 'About Me',
+    subtitle: "I'm a Full-Stack Developer from Pasadena, California.",
+    description:
+      'I enjoy the challenges brought to me and enjoy the complexities of the cyber world! ' +
+      'I love logic, problem solving, and coding within a cooperative team-oriented environment that allows me to grow. ' +
+      'I am well-versed in Javascript, HTML, CSS, C++, Java, NodeJS, React, and other Full Stack developer technologies.',
+    hobbies: [
+      'When I am not staring at lines of code and pixels, you\'ll find me snowboarding, in the gym, or taking excessive naps.'
+    ]
+  };
+  
   return (
-    <about>
-      {/* Add your About content here */}
-    </about>
+    <section id="about-me-content">
+      <h2>{aboutContent.title}</h2>
+      <h3>{aboutContent.subtitle}</h3>
+      <p>{aboutContent.description}</p>
+      <figure>
+        <img src={profilePic} alt="This is me!" />
+      </figure>
+      <ul>
+        {aboutContent.hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
+    </section>
   );
 }
 
--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import githubLogo from '../Assets/images/githublogo.png';
+import linkedinIcon from '../Assets/images/linkedinicon.png';
 
 function Contact() {
+  const links = [
+    {
+      title: 'Github Profile',
+      url: 'https://github.com/bryannguyen9',
+      image: githubLogo
+    },
+    {
+      title: 'LinkedIn Profile',
+      url: 'https://www.linkedin.com/in/bryannguyen9/',
+      image: linkedinIcon
+    },
+    {
+      title: 'Github.com',
+      url: 'https://github.com/',
+      image: githubLogo
+    }
+  ];
+
   return (
-    <contact>
-      {/* Add your Contact content here */}
-    </contact>
+    <section id="links-content">
+      <h2>Links</h2>
+      <nav>
+        <ul>
+          {links.map((link, index) => (
+            <figure className="links" id="links-card" key={index}>
+              <li>
+                <a href={link.url}>{link.title}</a>
+                <br />
+                <a href={link.url}>
+                  <img src={link.image} alt={link.title} />
+                </a>
+              </li>
+            </figure>
+          ))}
+        </ul>
+      </nav>
+    </section>
   );
 }
 
