fc34d64405c7242e3d32460b24c61b58e460414d
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-12-16 02:18:00 -0800

    updated portfolio page styling and animations

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import projects from '../data/projects.js';
 import Slider from 'react-slick';
 import cheerioImage from '../Assets/cheerio.png';
@@ -31,44 +31,63 @@ const getImageSource = (imageName) => {
   }
 };
 
-const ProjectCard = ({ project }) => (
-  <div className="project-card relative overflow-hidden p-4">
-    <a
-      href={project.liveSite || project.github}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div className="image-container" >
-        <img
-          src={getImageSource(project.image)}
-          alt={project.title}
-          className="w-full h-full"
-        />
-      </div>
-    </a>
-    <div className="project-info">
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <div className="links">
-        {project.liveSite && (
-          <a href={project.liveSite} target="_blank" rel="noopener noreferrer">
-            Live Site
-          </a>
+const ProjectCard = ({ project, currentSlide }) => {
+  const isMainProject = currentSlide === project.slideIndex;
+
+  return (
+    <div className={`project-card relative overflow-hidden p-4 group transition-transform duration-300 ease-in-out ${isMainProject ? 'main-project' : 'hover:scale-105'}`}>
+      <a
+        href={project.liveSite || project.github}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative block"
+      >
+        <div className="image-container" style={{ height: '250px' }}>
+          <img
+            src={getImageSource(project.image)}
+            alt={project.title}
+            className="w-auto h-full" // Set only width to maintain aspect ratio
+          />
+        </div>
+        {isMainProject && (
+          <div className="absolute bottom-4 left-0 w-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <div className="text-center">
+              <a
+                href={project.liveSite}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button mr-2"
+              >
+                Live Site
+              </a>
+              {project.github && (
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="button"
+                >
+                  GitHub
+                </a>
+              )}
+            </div>
+          </div>
         )}
-        {project.github && (
-          <a href={project.github} target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        )}
+      </a>
+      <div className={`project-info `}>
+        <h2 className={`text-4xl ${isMainProject ? 'transition-transform duration-500 transform scale-110' : 'transition-transform duration-500 transform scale-100'}`}>
+          {project.title}
+        </h2>
+        {/* Adjust the class and size as needed */}
+        <p>{project.description}</p>
       </div>
     </div>
-  </div>
-);
-
-
+  );
+};
 
 function Portfolio() {
   const sliderRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -90,6 +109,9 @@ function Portfolio() {
     focusOnSelect: true,
     autoplay: true,
     autoplaySpeed: 10000,
+    beforeChange: (oldIndex, newIndex) => {
+      setCurrentSlide(newIndex);
+    },
   };
 
   return (
@@ -97,7 +119,7 @@ function Portfolio() {
       <div className="w-full max-w-7xl">
         <Slider ref={sliderRef} {...settings}>
           {projects.map((project, index) => (
-            <ProjectCard key={index} project={project} />
+            <ProjectCard key={index} project={{ ...project, slideIndex: index }} currentSlide={currentSlide} />
           ))}
         </Slider>
       </div>
